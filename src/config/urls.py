a333eaf5a6f808863f7bbce85f0a7fb53336f83c
unohayato <mamidori1p1p@gmail.com>
2024-03-02 20:36:42 +0900

    :sparkles: add custom User

--- src/config/urls.py
+++ src/config/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from django.contrib.auth.decorators import login_required
 
 from django.views.generic import TemplateView
+from registration import views
 
 index_view = TemplateView.as_view(template_name="registration/index.html")
 
@@ -26,4 +27,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", login_required(index_view), name="index"),
     path('', include("django.contrib.auth.urls")),
+    path("signup/", views.SignUpView.as_view(), name="signup"),
 ]
